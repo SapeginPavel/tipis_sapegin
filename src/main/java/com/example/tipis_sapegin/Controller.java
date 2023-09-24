3ffd5e3077f51e5d33eae0221d70c5767bb7a9c6
Pavel Sapegin <pavelsapegin18@gmail.com>
2023-09-24 15:53:24 +0300

    A func createMeanderSeries was realised. Next step - add clearing of lineCharts

--- src/main/java/com/example/tipis_sapegin/Controller.java
+++ src/main/java/com/example/tipis_sapegin/Controller.java
@@ -3,12 +3,9 @@ package com.example.tipis_sapegin;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Pane;
 
 public class Controller {
@@ -39,12 +36,8 @@ public class Controller {
         System.exit(0);
     }
 
-    //понять, как передать график на отдельный виджет
-    //либо попытаться использовать какой-то виджет под размер 4 графиков чтоб
-    //либо через координаты хардкодить
-
     @FXML
-    void onClickBuildGraphs(ActionEvent event) {
+    void onClickBuildSinusGraphs(ActionEvent event) {
 
         //setAutoRanging(false)
         int maxX = 3;
@@ -57,9 +50,19 @@ public class Controller {
     }
 
     @FXML
+    void onClickBuildMeanderGraphs(ActionEvent event) {
+
+        int maxX = 3;
+
+        lch1.getData().add(CreateSeriesUtil.createMeanderSeries(maxX, 1));
+        lch2.getData().add(CreateSeriesUtil.createMeanderSeries(maxX, 2));
+        lch3.getData().add(CreateSeriesUtil.createMeanderSeries(maxX, 4));
+        lch4.getData().add(CreateSeriesUtil.createMeanderSeries(maxX, 8));
+    }
+
+    @FXML
     void initialize() {
         assert mainPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'ui.fxml'.";
-
 
     }
 
--- src/main/java/com/example/tipis_sapegin/CreateSeriesUtil.java
+++ src/main/java/com/example/tipis_sapegin/CreateSeriesUtil.java
@@ -4,12 +4,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.XYChart;
 
+import java.util.Arrays;
+
 public class CreateSeriesUtil {
 
     public static XYChart.Series createSinusSeries(int maxX, int sinusFrequency) {
 
-        XYChart.Series series = new XYChart.Series();
-        ObservableList<XYChart.Data> data = FXCollections.observableArrayList(); //в data надо добавлять новые XYChart.Data
+        ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
 
         int pointsForUnitSegment = 100;
 
@@ -20,7 +21,43 @@ public class CreateSeriesUtil {
                 break;
             }
         }
+
+        XYChart.Series series = new XYChart.Series();
         series.setData(data);
+
         return series;
+    }
+
+    public static XYChart.Series createMeanderSeries(int maxX, int meanderFrequency) {
+        ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
+
+        double step = (1 + 0.0) / meanderFrequency / 2;
+        int stepAmount = (int) (maxX / step) * 2;
+
+        double x = 0;
+        int y = 1;
+
+        double[] xx = new double[stepAmount];
+        double[] yy = new double[stepAmount];
+
+        for (int i = 0; i < stepAmount; i++) {
+            xx[i] = x;
+            yy[i] =  y;
+            data.add(new XYChart.Data(x, y));
+
+            if (i % 2 == 0) {
+                x += step;
+            }
+
+            if (i % 2 == 1) {
+                y = (y == 1 ? 0 : 1);
+            }
+        }
+
+        XYChart.Series series = new XYChart.Series();
+        series.setData(data);
+
+        return series;
+
     }
  }
